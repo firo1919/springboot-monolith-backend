5d3a2a0dfd6df1eda5565e2e1393cdf490716cde
Firomsa Assefa <firomsassf@gmail.com>
2026-07-07 22:39:45 +0300

    Fix: Update RustFS container wait strategy in E2E tests

--- src/test/java/com/firomsa/monolith/v1/controller/e2eTest/AbstractE2ETest.java
+++ src/test/java/com/firomsa/monolith/v1/controller/e2eTest/AbstractE2ETest.java
@@ -65,9 +65,7 @@ public abstract class AbstractE2ETest {
     private static final GenericContainer<?> s3 = new GenericContainer<>("rustfs/rustfs:latest")
             .withExposedPorts(9000, 9001).withEnv("RUSTFS_ACCESS_KEY", RUSTFS_ACCESS_KEY)
             .withEnv("RUSTFS_SECRET_KEY", RUSTFS_SECRET_KEY)
-            .waitingFor(org.testcontainers.containers.wait.strategy.Wait.forHttp("/")
-                    .forPort(9001) // Check the admin/UI port console
-                    .forStatusCode(200))
+            .waitingFor(org.testcontainers.containers.wait.strategy.Wait.forListeningPort())
             .withStartupTimeout(java.time.Duration.ofMinutes(3));
 
     static {
